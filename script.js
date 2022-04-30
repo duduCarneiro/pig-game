f0c970ad1d39f78b6353e4b04f19d27ff6539f09
Eduardo Carneiro <dumaniaoctavio@hotmail.com>
2022-04-30 12:53:41 -0300

    js2

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+
+const score0 = document.getElementById('score--0');
+const score1 = document.getElementById('score--1');
+
+
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
+
+
+const rollDice = document.querySelector('.btn--roll');
+const hold = document.querySelector('.btn--hold');
+const newGame  = document.querySelector('.btn--new');
+const dice  = document.querySelector('.dice');
