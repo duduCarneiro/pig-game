12cb456323e9fed59a68618961cd7023f49b8748
Eduardo Carneiro <dumaniaoctavio@hotmail.com>
2022-04-30 12:54:28 -0300

    js3

--- script.js
+++ script.js
@@ -14,3 +14,37 @@ const rollDice = document.querySelector('.btn--roll');
 const hold = document.querySelector('.btn--hold');
 const newGame  = document.querySelector('.btn--new');
 const dice  = document.querySelector('.dice');
+
+let valueScore0, valueScore1, valueCurrent;
+
+const init = function () {
+  dice.classList.add('hidden');
+  score0.textContent = 0;
+  score1.textContent = 0;
+  current0.textContent = 0;
+  current1.textContent = 0;
+  valueScore0 = 0;
+  valueScore1 = 0;
+  valueCurrent = 0;
+  rollDice.disabled = false;
+  hold.disabled = false;
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
+};
+
+newGame.addEventListener('click', init);
+
+rollDice.addEventListener('click', function () {
+  let diceNumber = Math.floor(Math.random() * 6 ) + 1;
+  dice.src = `dice-${diceNumber}.png`;
+  dice.classList.remove('hidden');
+
+  if (diceNumber === 1) {
+    switchPlayer();
+  } else {
+    valueCurrent += diceNumber;
+    player0.classList.contains('player--active') ? current0.textContent = valueCurrent : current1.textContent = valueCurrent;
+  };
+
+});
+
